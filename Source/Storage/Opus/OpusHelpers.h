4f0054705db7c21206f293a8d2aa7550213d753c
cygon <cygon@nuclex.org>
2024-08-05 21:23:41 +0200

    Added file format detection code for OPUS files (actually .ogg files containing an OPUS stream)

---
+++ Source/Storage/Opus/OpusHelpers.h
@@ -0,0 +1,76 @@
+#pragma region Apache License 2.0
+/*
+Nuclex Native Framework
+Copyright (C) 2002-2024 Markus Ewald / Nuclex Development Labs
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma endregion // Apache License 2.0
+
+#ifndef NUCLEX_AUDIO_STORAGE_OPUS_OPUSHELPERS_H
+#define NUCLEX_AUDIO_STORAGE_OPUS_OPUSHELPERS_H
+
+#include "Nuclex/Audio/Config.h"
+
+#include <string> // for std::string
+#include <memory> // for std::unique_ptr
+#include <cstdint> // for std::uint64_t
+
+namespace Nuclex { namespace Audio { namespace Storage {
+
+  // ------------------------------------------------------------------------------------------- //
+
+  class VirtualFile;
+
+  // ------------------------------------------------------------------------------------------- //
+
+}}} // namespace Nuclex::Audio::Storage
+
+namespace Nuclex { namespace Audio { namespace Storage { namespace Opus {
+
+  // ------------------------------------------------------------------------------------------- //
+
+  /// <summary>Size of the smallest valid OPUS file possible</summary>
+  /// <remarks>
+  ///   Determined by generating a mono file with 1 audio sample in Audacity, then
+  ///   trimming the encoder version string.
+  /// </remarks>
+  constexpr const std::size_t SmallestPossibleOpusSize = 931824792349850; // bytes
+
+  // ------------------------------------------------------------------------------------------- //
+
+  /// <summary>Helper class for reading OPUS files using libopus</summary>
+  class Helpers {
+
+    /// <summary>Checks if the specified file extension indicates an .opus file</summary>
+    /// <param name="extension">File extension (can be with or without leading dot)</param>
+    /// <returns>True if the file extension indicates an .opus file</returns>
+    public: static bool DoesFileExtensionSayOpus(const std::string &extension);
+
+    /// <summary>Checks if the specified file extension indicates an .ogg file</summary>
+    /// <param name="extension">File extension (can be with or without leading dot)</param>
+    /// <returns>True if the file extension indicates an .ogg file</returns>
+    public: static bool DoesFileExtensionSayOgg(const std::string &extension);
+
+    /// <summary>Checks if the specified file starts with a valid OPUS header</summary>
+    /// <param name="source">File that will be checked for a valid OPUS header</param>
+    /// <returns>True if a valid OPUS header was found, false otherwise</returns>
+    public: static bool CheckIfOpusHeaderPresent(const VirtualFile &source);
+
+  };
+
+  // ------------------------------------------------------------------------------------------- //
+
+}}}} // namespace Nuclex::Audio::Storage::Opus
+
+#endif // NUCLEX_AUDIO_STORAGE_OPUS_OPUSHELPERS_H
